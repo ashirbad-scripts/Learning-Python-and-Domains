09e84669556206c2a1d24c620afffc0fbb7b30a2
Ashirbad Routray <ashirbadroutray847@gmail.com>
2025-06-07 05:51:35 +0530

    Add files via upload

---
+++ Level_5/L5set_1.py
@@ -0,0 +1,34 @@
+# Create a string variable with your full name.
+fullName = "willow the Novice"
+print(fullName)
+
+# Print the length of a given string.
+print(len(fullName))
+
+# Access the first and last character of a string.
+print(fullName[0])
+print(fullName[-1])
+
+# Slice a string to get the first 5 characters.
+print(fullName[:5])
+
+# Slice and print every second character from a 
+# string.
+print(fullName[::2])
+
+# Convert a string to all uppercase letters.
+print(fullName.upper())
+
+# Convert a string to all lowercase letters.
+print(fullName.lower())
+
+# Replace all spaces in a string with hyphens (`-`).
+print(fullName.replace(" ", "-"))
+
+# Check if a string starts with a given prefix.
+print(fullName.startswith("will"))
+
+#  Check if a string ends with a specific suffix.
+print(fullName.endswith("Novice"))
+
+
