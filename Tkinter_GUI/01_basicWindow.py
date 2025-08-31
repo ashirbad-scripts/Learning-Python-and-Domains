7cabf6e2a6ab1facbfcb3ffb246933b6d96efd9c
Ashirbad Routray <ashirbadroutray847@gmail.com>
2025-08-31 06:05:34 +0530

    Learning tkiner Gui

---
+++ Tkinter_GUI/01_basicWindow.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+# Create main window
+root = tk.Tk()
+
+# Set window title
+root.title("My firts GUI")
+
+# Set window size
+root.geometry("400x300")
+
+# Start GUI
+root.mainloop()
